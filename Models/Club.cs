cb128f29cf9aa91d6f3f0c66623b68de7ed072d7
StevenFein <saf0929@gmail.com>
2022-11-14 21:45:32 -0800

    Added Club Model

---
+++ Models/Club.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_course_management.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        
+    }
+}
