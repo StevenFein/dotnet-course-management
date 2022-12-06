5129cb5be7824b8f44ad3d998db07dcf5089b790
StevenFein <saf0929@gmail.com>
2022-12-06 12:44:15 -0800

    Added initial db migration

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_course_management.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_course_management.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Course> Courses => Set<Course>();
+    }
+}
